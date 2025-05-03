f25fbe02a9f2e11c7b5a2d337b95c28ae37b0c6c
big4eer <why@big4eer.ru>
2025-05-04 01:24:22 +0300

    007_tip_calculator

---
+++ 006_chapter/002_variable_declaration/ex-01.js
@@ -0,0 +1,10 @@
+let username = 'big4eer';
+console.log(username);
+
+username = 'anonymous';
+console.log(username);
+
+const username1 = 'user01234';
+console.log(username1);
+
+console.log(username, username1);
